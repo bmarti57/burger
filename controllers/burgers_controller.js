8cb7611dd665936209cba73a832768f9b4ac65d5
Brian Martin <Brian Martin>
2017-09-06 19:54:33 -0400

    debug index

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
-    burger.all(function(data) {
+    burger.selectAll(function(data) {
         var tableData = {
             burgers: data
         };
@@ -15,7 +15,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("/", function(req, res) {
-    burger.create([
+    burger.insertOne([
         "burger_name", "devoured" 
     ], [
         req.body.burger_name, req.body.devoured
@@ -28,7 +28,7 @@ router.put("/:id", function(req, res) {
     var condition = "id = " + req.params.id;
     console.log("condition", condition);
 
-    burger.update({
+    burger.updateOne({
         devoured: req.body.devoured
     }, condition, function() {
         res.redirect("/");
