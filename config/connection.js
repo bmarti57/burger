abfed0703856b85e57dacb8c3e67f52b3282849a
Brian Martin <Brian Martin>
2017-09-07 19:17:18 -0400

    connection.js reset

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,14 @@
 var mysql = require("mysql");
-var connection;
 
-if(process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection ({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: burger_db
-  });
-};
+var connection = mysql.createConnection({
+  port: 3306,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "burger_db"
+});
 
+// Make connection.
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -20,4 +17,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+// Export connection for our ORM to use.
 module.exports = connection;
