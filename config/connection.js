503ab5b55e4887ff8a82e8702d9e7dfbc5d9b8cb
Brian Martin <Brian Martin>
2017-09-07 19:02:45 -0400

    connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,17 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burger_db"
-});
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection ({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: burger_db
+  });
+};
 
-// Make connection.
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -17,5 +20,4 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
 module.exports = connection;
